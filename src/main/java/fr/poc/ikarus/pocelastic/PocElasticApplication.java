cff24dd417386a5d0dd0421202c54c7b8de04394
Vabre ben <naruben74@hotmail.fr>
2023-11-30 15:47:16 +0100

    Initialisation

---
+++ src/main/java/fr/poc/ikarus/pocelastic/PocElasticApplication.java
@@ -0,0 +1,13 @@
+package fr.poc.ikarus.pocelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocElasticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocElasticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/poc/ikarus/pocelastic/PocElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.poc.ikarus.pocelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
